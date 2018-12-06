9a9aa315f410b8d1ff8f76e3ed3bd1071d927cfd
weixuanfu <weixuanf@pennmedicine.upenn.edu>
2018-12-06 15:05:16 -0500

    some fix

--- tpot/builtins/simple_autoencoder.py
+++ tpot/builtins/simple_autoencoder.py
@@ -7,36 +7,23 @@ from numpy import array
 from numpy import argmax
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-from sklearn.utils import check_array
+from sklearn.utils import check_array, check_X_y
+from sklearn.model_selection import train_test_split
 pd.options.mode.chained_assignment = None
 
 class SimpleAutoencoder(BaseEstimator, TransformerMixin):
-    
-    def __init__(self, x_train, y_train, x_val, y_val, encoding_dim, activation, optimizer, loss, epochs, batch_size):
-        self.x_train = x_train
-        self.X_width = self.x_train.shape[1]
-        self.y_train = y_train
-        self.x_val = x_val
-        self.y_val = y_val
+    def __init__(self, encoding_dim, activation, optimizer, loss, epochs, batch_size, random_state=42):
         self.encoding_dim = encoding_dim
         self.activation = activation
         self.optimizer = optimizer
         self.loss = loss
         self.epochs = epochs
         self.batch_size = batch_size
-        
-        self.input_placeholder = Input(shape = (self.X_width, ))    #input placeholder
-        self.encoded = Dense(self.encoding_dim, activation = self.activation)(self.input_placeholder)   #the encoded representation of input
-        self.decoded = Dense(self.X_width, activation = 'sigmoid')(self.encoded)
-        
-        #define autoencoder model object
-        self.autoencoder = Model(self.input_placeholder, self.decoded)
-        
-        #define separate encoder model object
-        self.encoder = Model(self.input_placeholder, self.encoded)      
-        
-        
-    def fit(self, X, y=None):
+        self.random_state = random_state
+
+
+
+    def fit(self, X, y):
         """Do nothing and return the estimator unchanged
         This method is just there to implement the usual API and hence
         work in pipelines.
@@ -44,21 +31,37 @@ class SimpleAutoencoder(BaseEstimator, TransformerMixin):
         ----------
         X : array-like
         """
-        X = check_array(X, accept_sparse='csr')
-        return self
-    
-    
-    def transform(self):
-        #compile autoencoder model
+        X, y = check_X_y(X, y, accept_sparse=True, dtype=None)
+        x_train, x_val, y_train, y_val = train_test_split(
+            X, y, test_size=0.25, train_size=0.75, random_state=self.random_state)
+
+        self.x_train = x_train
+        self.X_width = self.x_train.shape[1]
+        self.y_train = y_train
+        self.x_val = x_val
+        self.y_val = y_val
+        self.input_placeholder = Input(shape = (self.X_width, ))    #input placeholder
+        self.encoded = Dense(self.encoding_dim, activation = self.activation)(self.input_placeholder)   #the encoded representation of input
+        self.decoded = Dense(self.X_width, activation = 'sigmoid')(self.encoded)
+
+        #define autoencoder model object
+        """
+        self.autoencoder = Model(self.input_placeholder, self.decoded)
         self.autoencoder.compile(optimizer = self.optimizer, loss = self.loss)
-        
-        self.autoencoder.fit(self.x_train, self.x_train, 
-                             epochs = self.epochs, 
-                             batch_size = self.batch_size, 
-                             shuffle = True, 
-                             validation_data = (self.x_val, self.x_val))
-        encoded_preds = self.encoder.predict(self.x_val)
+        self.autoencoder.fit(self.x_train, self.x_train,
+                             epochs = self.epochs,
+                             verbose=0,
+                             batch_size = self.batch_size,
+                             shuffle = True,
+                             validation_data = (self.x_val, self.x_val))"""
+        #define separate encoder model object
+        self.encoder = Model(self.input_placeholder, self.encoded)
+        return self
+
+
+    def transform(self, X):
+        #compile autoencoder model
+        X = check_array(X, accept_sparse='csr')
+
+        encoded_preds = self.encoder.predict(X)
         return encoded_preds
-    
-  
-    
